b06fab823ffffb1169f770465b6cda4efa260cc4
joshuajhun <joshuajhun@gmail.com>
2018-01-26 21:20:00 -0700

    working on lazy loading container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokecards from '../../containers/Pokecards/Pokecards'
-import Pokestats from '../../containers/Pokestats/Pokestats'
+// import Pokecards from '../../containers/Pokecards/Pokecards'
+// import Pokestats from '../../containers/Pokestats/Pokestats'
 
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      pokestats: null,
+      PokeCards: null
+      loading: true
+    }
   }
+
+  async componentDidMount() {
+    const lazyPokeStats = await import('../../containers/Pokecards/Pokecards')
+    const { default: PokeCards} = lazyPokeStats
+    this.setState({ PokeCards, loading: false })
+  }
+
   render() {
+  const { pokestats, PokeCards } = this.state
+  const ren = PokeCards ? < PokeCards />
+                        : 'why tho'
     return (
       <div className='App'>
         <h1 className='header'> POKéDEX </h1>
-        <Pokecards />
-        <Pokestats />
+        {ren}
+        {/* <Pokestats /> */}
       </div>
     );
   }
